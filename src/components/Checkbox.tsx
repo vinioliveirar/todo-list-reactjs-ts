b43ea621970aec16203fc2cf3ac5552dfb148ca7
Vinicius Oliveira Rocha <vinicius@Mac-mini-de-Stefany.local>
2022-12-07 18:13:13 -0300

    fcriacao checkbox e ajuestes componentes

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,22 @@
+import { CheckCircle, Circle } from "phosphor-react";
+import { ButtonHTMLAttributes } from "react";
+
+import styles from "./Checkbox.module.css";
+interface CheckboxProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  checked?: boolean;
+}
+export function Checkbox({ checked, ...props }: CheckboxProps) {
+  return (
+    <>
+      {checked ? (
+        <button className={styles.checkbox} {...props}>
+          <Circle size={24} />
+        </button>
+      ) : (
+        <button className={styles.checkboxChecked} {...props}>
+          <CheckCircle size={24} />
+        </button>
+      )}
+    </>
+  );
+}
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -2,20 +2,23 @@ import { ChangeEvent, FormEvent, HtmlHTMLAttributes, useState } from "react";
 import { v4 } from "uuid";
 import { ListTaks } from "./ListTask";
 import styles from "./Task.module.css";
+import Clipboard from "../assets/Clipboard.svg";
 
 export interface Task {
   id: string;
   content: string;
+  completed: boolean;
 }
 
 export function Task() {
   const [tasks, setTaks] = useState<Task[]>([]);
+  const [completedTask, setCompletedTask] = useState(0);
 
   const [newTaskText, setNewTaskText] = useState("");
 
   function handleCreateNewTaks(event: FormEvent) {
     event.preventDefault();
-    setTaks([...tasks, { id: v4(), content: newTaskText }]);
+    setTaks([...tasks, { id: v4(), content: newTaskText, completed: false }]);
     setNewTaskText("");
   }
 
@@ -29,6 +32,25 @@ export function Task() {
       return task.content !== taskToDelete;
     });
     setTaks(tasksWithoutDeletedOne);
+  }
+
+  function handleCheckTask(id: string) {
+    const newTaskList = tasks
+      .map((task) => {
+        if (task.id === id) {
+          task.completed = !task.completed;
+        }
+
+        return task;
+      })
+      .sort((a, b) => Number(a.completed) - Number(b.completed));
+
+    setTaks(newTaskList);
+    const seeCompletedTasks = tasks.filter((task) => {
+      return task.completed;
+    });
+
+    setCompletedTask(seeCompletedTasks.length);
   }
 
   return (
@@ -52,16 +74,36 @@ export function Task() {
             <span className={styles.counter}>{tasks.length}</span>
           </p>
           <p className={styles.done}>
-            Concluidas <span className={styles.counter}></span>
+            Concluidas{" "}
+            <span className={styles.counter}>
+              {completedTask} de {tasks.length}
+            </span>
           </p>
         </header>
-        <section className={styles.contentListTask}>
-          {tasks.map((task) => {
-            return (
-              <ListTaks key={task.id} task={task} onDeleteTask={deleteTask} />
-            );
-          })}
-        </section>
+
+        {tasks.length === 0 ? (
+          <div className={styles.noTasks}>
+            <img src={Clipboard} alt="Clipboard" />
+            <p>
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+            </p>
+            <p>Crie tarefas e organize seus itens a fazer</p>
+          </div>
+        ) : (
+          <section className={styles.contentListTask}>
+            {tasks.map((task) => {
+              return (
+                <ListTaks
+                  key={task.id}
+                  task={task}
+                  onDeleteTask={deleteTask}
+                  onCheckTask={handleCheckTask}
+                  completedTaks={task.completed}
+                />
+              );
+            })}
+          </section>
+        )}
       </div>
     </div>
   );
